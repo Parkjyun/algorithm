d6e4488e05d000103d05e7e3bd9348e88b229618
Parkjyun <jaeyun0507@naver.com>
2025-02-18 16:22:31 +0900

    programmers 전화번호목록

---
+++ src/programmers/전화번호목록.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class 전화번호목록 {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
+
+        for(String str : phone_book) {
+            for(int i = 1; i < str.length(); i++) {
+                if(set.contains(str.substring(0,i))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
