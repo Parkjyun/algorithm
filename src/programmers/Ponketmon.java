dc6072c1f9e6eec75e9847e9c4f80db3215ad7c8
Parkjyun <jaeyun0507@naver.com>
2025-02-18 10:00:48 +0900

    programmers ponketmon

---
+++ src/programmers/Ponketmon.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ponketmon {
+        public int solution(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i = 0; i < nums.length; i++) {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
+            int chance = nums.length/2; //고를 수 있는 마리
+            int variaty = map.size();
+            if(variaty >= chance) return chance;
+            return variaty;
+        }
+}
