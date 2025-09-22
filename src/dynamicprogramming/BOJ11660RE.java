cccf04fb17f4f1f8ce088e1fae23e24415b9037e
Parkjyun <jaeyun0507@naver.com>
2025-09-22 17:22:34 +0900

    boj 11660

---
+++ src/dynamicprogramming/BOJ11660RE.java
@@ -0,0 +1,47 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11660RE {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] ps = new int[n+1][n+1];
+        int[][] map = new int[n+1][n+1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                ps[i][j] = map[i][j] + ps[i][j-1];
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                ps[i][j] = ps[i][j] + ps[i-1][j];
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(ps[x2][y2] + ps[x1-1][y1-1] - ps[x2][y1-1] - ps[x1-1][y2]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
